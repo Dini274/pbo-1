e48a8c2cb534fb7a340327eb62c2eb2d299a7acd
Dini274 <77830581+Dini274@users.noreply.github.com>
2021-09-25 11:30:14 +0700

    Add files via upload

---
+++ pbo1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java1.pkg1;
+
+/**
+ *
+ * @author Dini Aryani
+ */
+public class pbo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+}
+    
+
---
+++ pbo2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java1.pkg1;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author dini aryani
+ */
+public class pbo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+    
+}
