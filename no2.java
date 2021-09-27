3e37e34be69a241e7e8c813999846430d74a6597
Dini274 <77830581+Dini274@users.noreply.github.com>
2021-09-27 10:55:39 +0700

    Add files via upload

---
+++ no2.java
@@ -0,0 +1,13 @@
+
+package pbo;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Dini Aryani
+ */
+public class no2 {
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
